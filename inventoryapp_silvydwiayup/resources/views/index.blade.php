86aa6ee18a23d9e3abc719167bab1c8878e13790
silvydwiayup <100912347+silvydwiayup@users.noreply.github.com>
2025-10-30 07:59:28 +0700

    commit tugas 13

--- inventoryapp_silvydwiayup/resources/views/index.blade.php
+++ inventoryapp_silvydwiayup/resources/views/index.blade.php
@@ -1,32 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>SanberBook</h1>
-    <h3>Social Media Developer Santai Berkualitas</h3>
-    <p>Belajar dan Berbagi agar hidup ini semakin berkualitas</p>
-
-    <div class="benefit">
-        <h3>Benefit Join di SanberBook</h3>
-        <ul>
-            <li>Mendapatkan motivasi dari sesama developer</li>
-            <li>Sharing knowledge dari para mastah sanber</li>
-            <li>Dibuat oleh calon web developer terbaik</li>
-        </ul>
-    </div>
-
-    <div class="cara-bergabung">
-        <h3>cara bergabung ke SanberBook</h3>
-        <ul>
-            <li>Mengunjungi Website ini</li>
-            <li>Mendaftar di <a href="/register">Form Sign Up</a></li>
-            <li>Selesai!</li>
-        </ul>
-    </div>
+@extends('home')
+@section('header', 'Dashboard')
     
-</body>
-</html>
+@section('bodyContent')
+    
+<h1>SanberBook</h1>
+<h3>Social Media Developer Santai Berkualitas</h3>
+<p>Belajar dan Berbagi agar hidup ini semakin berkualitas</p>
+
+<div class="benefit">
+    <h3>Benefit Join di SanberBook</h3>
+    <ul>
+        <li>Mendapatkan motivasi dari sesama developer</li>
+        <li>Sharing knowledge dari para mastah sanber</li>
+        <li>Dibuat oleh calon web developer terbaik</li>
+    </ul>
+</div>
+
+<div class="cara-bergabung">
+    <h3>cara bergabung ke SanberBook</h3>
+    <ul>
+        <li>Mengunjungi Website ini</li>
+        <li>Mendaftar di <a href="/register">Form Sign Up</a></li>
+        <li>Selesai!</li>
+    </ul>
+</div>
+@endsection
--- inventoryapp_silvydwiayup/resources/views/welcome.blade.php
+++ inventoryapp_silvydwiayup/resources/views/welcome.blade.php
@@ -1,12 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Selamat Datang, {{ $firstname }} {{ $lastname }}!</h1>
-    <h2>Terimakasih telah bergabung di Sanberbook. Social Media kita bersama!</h2>
-</body>
-</html>
+@extends('/home')
+@section('header', 'welcome')
+@section('bodyContent')
+
+<h1>Selamat Datang, {{ $firstname }} {{ $lastname }}!</h1>
+<h2>Terimakasih telah bergabung di Sanberbook. Social Media kita bersama!</h2>
+@endsection
