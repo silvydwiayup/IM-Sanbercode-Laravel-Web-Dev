95bbd038769142d664e31577cae69b6930e1964b
silvydwiayup <100912347+silvydwiayup@users.noreply.github.com>
2025-10-31 08:59:37 +0700

    commit tugas 14

---
+++ inventoryapp_silvydwiayup/database/migrations/2025_10_31_014548_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('products_id');
+            $table->foreign('products_id')->references('id')->on('products')->onDelete("cascade")->onUpdate("cascade");
+            $table->unsignedBigInteger('users_id');
+            $table->foreign('users_id')->references('id')->on('users')->onDelete("cascade")->onUpdate("cascade");
+
+            $table->enum('type', ['in', 'out']);
+            $table->integer('amount');
+            $table->text('notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
