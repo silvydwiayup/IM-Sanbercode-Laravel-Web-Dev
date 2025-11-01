b6132ce15a4948af630547c2df989d4244d7c71f
silvydwiayup <100912347+silvydwiayup@users.noreply.github.com>
2025-11-01 15:27:12 +0700

    add tugas 15

---
+++ inventoryapp_silvydwiayup/resources/views/categories/tambah.blade.php
@@ -0,0 +1,29 @@
+@extends('home')
+@section('header', "Tambah Category")
+@section('bodyContent')
+
+<form action="/categories" method="POST">
+    @csrf
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+  <div class="mb-3">
+    <label class="form-label">Category Name</label>
+    <input type="text" name="name" class="form-control" value="{{ old('name') }}">
+  </div>
+  <div class="mb-3">
+    <label class="form-label" class="form-label">Category Description</label>
+    <textarea name="description" class="formo-control" id="" cols="30" rows="10" {{old('description')}}></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    
+@endsection
