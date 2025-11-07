9574c927559789a33b0f09a13de6728d4498f165
Valentina Resha <silvydwiap88@gmail.com>
2025-11-07 11:14:27 +0700

    add tampilan Final Project

--- inventoryapp_silvydwiayup/routes/web.php
+++ inventoryapp_silvydwiayup/routes/web.php
@@ -5,12 +5,9 @@ use App\Http\Controllers\Index;
 use App\Http\Controllers\Register;
 use App\Http\Controllers\Welcome;
 use App\Http\Controllers\CategoriesController;
-<<<<<<< HEAD
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\TransactionController;
-=======
->>>>>>> b6132ce15a4948af630547c2df989d4244d7c71f
 
 Route::get('/', [Index::class, 'index']);
 
@@ -22,11 +19,8 @@ Route::post('/login', [LoginController::class, 'auth']);
 
 Route::get('/welcome', [Welcome::class, 'welcome']);
 
-<<<<<<< HEAD
 // Categories
 
-=======
->>>>>>> b6132ce15a4948af630547c2df989d4244d7c71f
 Route::get('/categories/create', [CategoriesController::class, 'create']);
 Route::post('/categories', [CategoriesController::class, 'store']);
 
@@ -38,7 +32,6 @@ Route::get('/categories/{id}/edit', [CategoriesController::class, 'edit']);
 Route::put('/categories/{id}', [CategoriesController::class, 'update']);
 
 
-<<<<<<< HEAD
 Route::delete('/categories/{id}', [CategoriesController::class, 'destroy']);
 
 // Products
@@ -54,7 +47,3 @@ Route::get('/products/show', [ProductController::class, 'show']);
 Route::get('/transaction', [TransactionController::class, 'index']);
 Route::get('/transaction/add', [TransactionController::class, 'add']);
 
-
-=======
-Route::delete('/categories/{id}', [CategoriesController::class, 'destroy']);
->>>>>>> b6132ce15a4948af630547c2df989d4244d7c71f
