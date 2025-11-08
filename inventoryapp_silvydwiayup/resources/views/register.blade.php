c92d585c42ec4d5c2a6b47dd480245ee98cbbdb8
Valentina Resha <silvydwiap88@gmail.com>
2025-11-08 21:34:28 +0700

    add Final Project

--- inventoryapp_silvydwiayup/resources/views/register.blade.php
+++ inventoryapp_silvydwiayup/resources/views/register.blade.php
@@ -59,7 +59,7 @@
                   <input type="submit" class="btn btn-primary w-100 py-8 fs-4 mb-4" value="Sign Up">
                   <div class="d-flex align-items-center justify-content-center">
                     <p class="fs-4 mb-0 fw-bold">Already have an Account?</p>
-                    <a class="text-primary fw-bold ms-2" href="">Sign In</a>
+                    <a class="text-primary fw-bold ms-2" href="/login">Sign In</a>
                   </div>
                 </form>
 
